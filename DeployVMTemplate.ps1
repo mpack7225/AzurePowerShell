81e3ddea3ea4d1584d0d1a18f6c1ee4f1c5fd173
mpack7225 <mpack@precisionlender.com>
2018-02-18 21:46:31 -0500

    Script for deploying a new Azure VM

---
+++ DeployVMTemplate.ps1
@@ -0,0 +1,70 @@
+# Load Dependencies
+."$PSScriptRoot\vm.localConfig.ps1"
+Write-Host "Deploying Virtual Machine $environment"
+
+# Check for Login and Load Parameters
+Write-Host "$(Get-Date) Logging into Azure."
+Try {
+    $currentSubscription = (Get-AzureRmContext -ErrorAction Continue).Subscription.Id
+    if($currentSubscription -ne $SubscriptionId) {
+        $currentSubscription = $subscriptionId
+        Login-AzureRmAccount
+        Set-AzureRmContext -SubscriptionId $currentSubscription
+    }
+} Catch [System.Management.Automation.PSInvalidOperationException] {
+    Login-AzureRmAccount
+    Set-AzureRmContext -SubscriptionId $currentSubscription
+} 
+
+if(!$location)
+{
+    $location = Read-Host -Prompt "Please Provide a Project Location"
+}
+
+if(!$country)
+{
+    $country = Read-Host -Prompt "Please Provide a Project Country"
+}
+
+if(!$projectName)
+{
+    $projectName = Read-Host -Prompt "Please Provide the Project Name"
+}
+
+$projectName = $projectName.ToUpper()
+
+if(!$projectNumber)
+{
+    $projectNumber = Read-Host -Prompt "Please Provide the Project Number"
+}
+
+
+Set-Location -Path $PSScriptRoot
+
+$rgName = "GROUP-$country-$projectName-$projectNumber"
+
+# Create Resource Group (if it exists this wont re-create it)
+New-AzureRmResourceGroup -Name $rgName -Location $location -Force
+
+# Deploy the Azure Virtual Machine (and dependancies) resources
+(Get-Date).DateTime
+
+$deploymentparms = @{
+    ResourceGroupName = $rgName
+    TemplateFile = $templatefilelocation
+    adminPassword = $vmAdminPassword
+    storageAccountPrefix = $storageAccountPrefix
+    projectName = $projectName
+    projectNumber = $projectNumber
+    location = $location
+    country = $country
+    subscriptionId = $SubscriptionId
+    vmSize = $vmSize
+    ipPrefix = $ipPrefix
+
+}
+
+$resourceDeploy = New-AzureRmResourceGroupDeployment  @deploymentparms
+
+(Get-Date).DateTime
+
